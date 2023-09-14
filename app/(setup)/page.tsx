0a7f9ea0f5ee87d79991534a8f115ee432887b70
Hassaan-Coder <hassaansultan720@gmail.com>
2023-09-14 12:24:23 +0500

    make the inital form to create workspace

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,27 @@
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
+import { InitialModal } from "@/components/modals/initial-modal";
+
+const SetupPage = async () => {
+  const profile = await initialProfile();
+
+  const workSpace = await db.workSpace.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (workSpace) {
+    return redirect(`/workSpaces/${workSpace.id}`);
+  }
+
+  return <InitialModal />;
+};
+
+export default SetupPage;
